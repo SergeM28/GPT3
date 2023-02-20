78f042a7dd9a91921013567e01553a56dc8fbcd8
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-02-20 13:27:15 +0300

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Footer, Blog, Posibility, Features, WhatGPT3, Header } from './containers';
-import { Navbar, CTA, Brand} from './components';
+import { Navbar, CTA, Brand } from './components';
+import './App.css';
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Posibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
     )
 }
 
